de1b34ead06fca64e8ac39378aff0ff1fbcb70ed
Mazen <mazin61192@gmail.com>
2023-08-07 09:58:03 +0300

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,17 +2,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController:ControllerBase
+   
+    public class ProductsController:BaseApiController
     {
         //for repository
        /* private readonly IProductRepository _repo;   
@@ -64,6 +65,8 @@ namespace API.Controllers
            return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id) //ProductToReturnDto was product before dto
         {
             //return await _productsRepo.GetByIdAsync(id); for generic before specification
@@ -81,6 +84,7 @@ namespace API.Controllers
                 ProductType=product.ProductType.Name
             };
             before automapper*/
+            if(product == null ) return NotFound(new ApiResponse(404));
             return _mapper.Map<Product,ProductToReturnDto>(product);
         }
 
